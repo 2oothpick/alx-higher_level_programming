711ea3456e493580339c8366ed03765eef7cc7fd
2oothpick <kobbyboateng14@gmail.com>
2022-11-01 06:58:54 +0000

    done and done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,4 +7,4 @@ b = 5
 print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
 print("{:d} - {:d} = {:d}".format(a, b, sub(a,b)))
 print("{:d} * {:d} = {:d}".format(a, b, mul(a,b)))
-print("{:d} / {:d} = {:d}".format(a, b, div(a,b)))
+print("{:d} / {:d} = {:d} \n".format(a, b, div(a,b)))
