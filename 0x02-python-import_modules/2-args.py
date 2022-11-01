a57111191d625fbe4a5392f6bea10e3e106212ca
2oothpick <kobbyboateng14@gmail.com>
2022-11-01 08:39:08 +0000

    done with 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     import sys
     number = len(sys.argv) - 1
 
+
     if number == 0:
         print("0 arguments.")
     elif number == 1:
@@ -12,4 +13,3 @@ if __name__ == "__main__":
 
     for i in range(number):
         print("{}: {}".format((i + 1), sys.argv[i + 1]))
-        
