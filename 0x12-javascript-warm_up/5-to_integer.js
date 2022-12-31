5c39c7e8b483098d99da2efbeb25f9f4288356a1
2oothpick <kobbyboateng14@gmail.com>
2022-12-31 18:42:25 +0000

    to_integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let x = Number(process.argv[2]);
-if (isNaN(x)){
-    console.log("Not a number")
+const x = Number(process.argv[2]);
+if (isNaN(x)) {
+  console.log('Not a number');
 } else {
-    console.log(`My number: ${x}`);
+  console.log(`My number: ${x}`);
 }
