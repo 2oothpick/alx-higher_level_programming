600f97d74b15907d88d7d244c8350bf54b29d1c1
2oothpick <kobbyboateng14@gmail.com>
2022-12-31 18:59:50 +0000

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let a = Number(process.argv[2]);
-let b = Number(process.argv[3]);
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
 
-function add(a,b){
-    console.log(a+ b);
+function add (a, b) {
+  console.log(a + b);
 }
-add(a,b);
+add(a, b);
