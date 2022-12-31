50be32223166c7df706a9a0af3d3a1bf6dc2cd84
2oothpick <kobbyboateng14@gmail.com>
2022-12-31 19:05:25 +0000

    second_biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-let myArray = [];
-if (process.argv.length <= 3){
-    console.log(0);
+const myArray = [];
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-    for (x = 2; process.argv[x] !== undefined; x++){       //takes arguments from CL and puts them ina useable array
-        myArray.push(Number(process.argv[x]));             //converts arguments to numbers and concatenates to myArray                         
-    }
-    let sortedArray = myArray.sort();
-    const secondBiggest = sortedArray.length - 2;    //index position of the second biggest value in sortedArray
-    console.log(sortedArray[secondBiggest]);
+  for (let x = 2; process.argv[x] !== undefined; x++) {// takes arguments from CL and puts them ina useable array
+      myArray.push(Number(process.argv[x]));             // converts arguments to numbers and concatenates to myArray                         
+  }
+  let sortedArray = myArray.sort();
+  const secondBiggest = sortedArray.length - 2;    // index position of the second biggest value in sortedArray
+  console.log(sortedArray[secondBiggest]);
 }
