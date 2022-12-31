c923eb2962bd9337539aa120ea682828d360d627
2oothpick <kobbyboateng14@gmail.com>
2022-12-31 19:14:55 +0000

    call_me_moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-
-//defining the fxn
-callMeMoby = function (x, theFunction){
-    for(let i = 0; i < x; i++) {
-        theFunction();
-    }
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
 };
-
-//making the fxn visible from outside
-module.exports = {callMeMoby};
