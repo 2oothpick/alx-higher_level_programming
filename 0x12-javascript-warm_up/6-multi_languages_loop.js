8b0ffa5c3815bcef666c94db3386a74adaac7d94
2oothpick <kobbyboateng14@gmail.com>
2022-12-31 18:46:20 +0000

    bilingual

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let lists = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (list of lists){
-    console.log(list);
+const lists = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (x of lists) {
+  console.log(x);
 }
