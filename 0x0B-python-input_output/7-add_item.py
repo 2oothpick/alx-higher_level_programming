77b621dfdf5f0ad0bd2a0e094e10d11cc5cd651e
2oothpick <kobbyboateng14@gmail.com>
2023-02-18 16:03:39 +0000

    add file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,8 +10,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 argv_list = []
-for i in range(1, len(sys.argv)):
-    argv_list.append(sys.argv[i])
 if path.isfile("add_file.json"):
     argv_list = load_from_json_file("add_file.json")
+for i in range(1, len(sys.argv)):
+    argv_list.append(sys.argv[i])    
 save_to_json_file(argv_list,"add_file.json" )
