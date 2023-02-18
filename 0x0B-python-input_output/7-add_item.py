e6c5431f099d0f4f519f6032be54c8c73297ef57
2oothpick <kobbyboateng14@gmail.com>
2023-02-18 16:00:19 +0000

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,12 +5,13 @@ this module contains the add_item function
 
 
 import sys
+from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 argv_list = []
 for i in range(1, len(sys.argv)):
     argv_list.append(sys.argv[i])
-filename = 'add_item.json'
-save_to_json_file(argv_list, filename)
-load_from_json_file(filename)
+if path.isfile("add_file.json"):
+    load_from_json_file("add_file.json")
+save_to_json_file(argv_list,"add_file.json" )
