2b3ab19180926e2936655918880583641d6a12e5
2oothpick <kobbyboateng14@gmail.com>
2023-02-18 15:44:30 +0000

    add to file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+this module contains the add_item function
+"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+argv_list = []
+for i in range(1, len(sys.argv)):
+    argv_list.append(sys.argv[i])
+filename = 'add_item.json'    
+save_to_json_file(argv_list, filename)
+load_from_json_file(filename)
