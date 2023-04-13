258d6e87031e4394a94722b6a535765cd1a1a6ba
2oothpick <kobbyboateng14@gmail.com>
2023-04-13 08:48:47 +0000

    matrix_divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,21 +18,22 @@ def matrix_divided(matrix, div):
         raise TypeError('matrix must be a matrix (list of lists)\
  of integers/floats')
 
-    number_of_rows = len(matrix)
-#   delimiter serves as a bound for row comparison
-#   this prevents the comparison from going out of range
-    delimiter = number_of_rows - 1
-    for x in range(delimiter):
-        #   raising TypeError if rows in the matrix aren't
-        #   of the same size
-        if len(matrix[x]) != len(matrix[x+1]):
-            raise TypeError('Each row of the matrix must\
- have the same size')
     for i in matrix:
-        #   raises TypeError matrix isn't a list of a list
+        #   raises TypeError if element in matrix aren't a list
         if type(i) != list:
             raise TypeError('matrix must be a matrix\
  (list of list) of integers/floats')
+        else:
+            number_of_rows = len(matrix)
+            #   delimiter serves as a bound for row comparison
+            #   this prevents the comparison from going out of range
+            delimiter = number_of_rows - 1
+        for x in range(delimiter):
+            #   raising TypeError if rows in the matrix aren't
+            #   of the same size
+            if len(matrix[x]) != len(matrix[x+1]):
+                raise TypeError('Each row of the matrix must\
+ have the same size')
         inner_list = []
         for j in range(len(i)):
             #   raising TypeError when elements in the matrix
