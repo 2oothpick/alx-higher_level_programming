1445a41dca7ef84583ffaf4be3c75e3e1d169474
2oothpick <kobbyboateng14@gmail.com>
2023-04-13 11:56:15 +0000

    text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module contains the ``print_square`` function
+"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines aftereach of
+    these characters: . , ? and :"""
+    if type(text) != str:
+        raise TypeError('text must be a string')
+    else:
+        for i in text:
+            if (i == '.') or (i == '?') or (i == ':'):
+                print('\n')
+            else:
+                print(i, end = '')
