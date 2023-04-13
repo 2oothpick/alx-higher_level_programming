ea45ccea4c32eeee1f0254dda7b0f1dfd908cdc6
2oothpick <kobbyboateng14@gmail.com>
2023-04-13 06:00:41 +0000

    matrix_divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,26 +15,30 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError('division by zero')
     #raising TypeError if matrix isnt a list
     if type(matrix) != list:
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        raise TypeError('matrix must be a matrix (list of lists)\
+                         of integers/floats')
 
     number_of_rows = len(matrix)
     #delimiter serves as a bound for row comparison
     delimiter = number_of_rows - 1
     for x in range(delimiter):
         if len(matrix[x]) != len(matrix[x+1]):
-            raise TypeError('Each row of the matrix must have the same size')        
+            raise TypeError('Each row of the matrix must\
+                             have the same size')
     
     for i in matrix:
         #raising TypeError if matrix is not a list of a list
         if type(i) != list:
-            raise TypeError('matrix must be a matrix (list of list) of integers/floats')
+            raise TypeError('matrix must be a matrix\
+                             (list of list) of integers/floats')
         
         inner_list = []
         for j in range(len(i)):
             #raising a TypeError if elements in the matrix
             #are not integers/floats
-            if type(i[j]) not in [int,float]:
-                raise TypeError('matrix must be a matrix (list of list) of integers/floats')
+            if type(i[j]) not in [int, float]:
+                raise TypeError('matrix must be a matrix \
+                                (list of list) of integers/floats')
             
             quotient = f" {i[j]/div:.2f}"
             inner_list.append(quotient)
