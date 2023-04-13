771217ee35d74fde1cad8937bca0782eccd6865e
2oothpick <kobbyboateng14@gmail.com>
2023-04-13 06:22:38 +0000

    matrix_divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,33 +7,33 @@ Module contains a matrix division function
 def matrix_divided(matrix, div):
     """Divides elements in matrix by div """
     outer_list = []
-    ##raising TypeError if div is not a number
+#    raising TypeError if div is not a number
     if type(div) not in [int, float]:
         raise TypeError('div must be a number')
-    ##raising TypeError if div is zero
+#    raising TypeError if div is zero
     if div == 0:
         raise ZeroDivisionError('division by zero')
-    ##raising TypeError if matrix isnt a list
+#   raising TypeError if matrix isnt a list
     if type(matrix) != list:
         raise TypeError('matrix must be a matrix (list of lists)\
  of integers/floats')
 
     number_of_rows = len(matrix)
-    ##delimiter serves as a bound for row comparison
+#    delimiter serves as a bound for row comparison
     delimiter = number_of_rows - 1
     for x in range(delimiter):
         if len(matrix[x]) != len(matrix[x+1]):
             raise TypeError('Each row of the matrix must\
  have the same size')
     for i in matrix:
-        ##raising TypeError if matrix is not a list of a list
+#       raising TypeError if matrix is not a list of a list
         if type(i) != list:
             raise TypeError('matrix must be a matrix\
 (list of list) of integers/floats')
         inner_list = []
         for j in range(len(i)):
-            ##raising a TypeError if elements in the matrix
-            ##are not integers/floats
+#            raising a TypeError if elements in the matrix
+#            are not integers/floats
             if type(i[j]) not in [int, float]:
                 raise TypeError('matrix must be a matrix \
 (list of list) of integers/floats')
