dffa49bc0cc4d2d4f82a26244c87393f62961276
2oothpick <kobbyboateng14@gmail.com>
2023-04-14 01:53:00 +0000

    text_indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,11 +7,20 @@ Module contains the ``print_square`` function
 def text_indentation(text):
     """Prints a text with 2 new lines aftereach of
     these characters: . , ? and :"""
+    counter = 0
+    #   counter is is assigned '1' right after a punctuation
     if type(text) != str:
         raise TypeError('text must be a string')
     else:
         for i in text:
             if (i == '.') or (i == '?') or (i == ':'):
-                print(f'{i}\n')
+                print(f'{i}')
+                counter = 1
+                print()
             else:
-                print(i, end = '')      
+                if i == ' ' and counter == 1:
+                    i == i.lstrip()
+                    counter = 0
+                    continue
+                if i:
+                    print(i, end='')
