52e0c358b8665f232d887949506ac66f85581bc5
2oothpick <kobbyboateng14@gmail.com>
2023-04-14 03:01:23 +0000

    unit test for max integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,6 +1,9 @@
 import unittest 
-max_integer_test = __import__('6-max_integer_test').max_integer_test
+max_integer = __import__('6-max_integer_test').max_integer_test
 
 class TestMaxInteger(unittest.TestCase):
     """Unit tests for `max_integer_test`"""
-    
+    def test_positive_beginning(self):
+        """Tests for all positive with max at beginning"""
+        max = [200, 10, 8, 36, 14, 50]
+        self.assertEqual(max_integer(max), 200)
