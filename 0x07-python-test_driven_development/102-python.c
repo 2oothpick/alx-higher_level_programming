ffb873d871f14facf41a7ca4303aab8bd197208b
2oothpick <kobbyboateng14@gmail.com>
2023-04-15 20:34:49 +0000

    adding additiona headers

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,4 +1,7 @@
 #include "Python.h"
+#include <stdio.h>
+#include <string.h>
+#include <Python.h>
 
 /**
  * print_python_string - Prints information about Python strings.
@@ -25,4 +28,4 @@ void print_python_string(PyObject *p)
 		printf("  type: compact unicode object\n");
 	printf("  length: %ld\n", length);
 	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
-
+}
