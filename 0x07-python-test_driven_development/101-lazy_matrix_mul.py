4bf338a231a1e367dba995e0fc8988b94a01d030
2oothpick <kobbyboateng14@gmail.com>
2023-04-15 20:29:01 +0000

    test for lazy_matrix_mul

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
+# 101-lazy_matrix_mul.py
 """Defines a matrix multiplication function using NumPy."""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices.
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
-    """
-    # exception raised for m_a not being a list of a list
-    for i in m_a:
-        if type(i) is not list:
-            raise ValueError('shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)')
-    #   exception for m_b not being list of list
-    for i in m_b:
-        if type(i) is not list:
-            raise ValueError('shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)')
+    """Return the multiplication of two matrices."""
     return (np.matmul(m_a, m_b))
