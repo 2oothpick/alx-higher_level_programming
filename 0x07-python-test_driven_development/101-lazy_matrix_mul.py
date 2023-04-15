1c57c5e6723aeced240dd211ad4917b38da8e1f6
2oothpick <kobbyboateng14@gmail.com>
2023-04-15 20:25:23 +0000

    test for lazy_matrix_mul

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,4 +9,12 @@ def lazy_matrix_mul(m_a, m_b):
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
     """
+    # exception raised for m_a not being a list of a list
+    for i in m_a:
+        if type(i) is not list:
+            raise ValueError('shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)')
+    #   exception for m_b not being list of list
+    for i in m_b:
+        if type(i) is not list:
+            raise ValueError('shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)')
     return (np.matmul(m_a, m_b))
