810c3ac8aadcbd7947518900ad5d36d15123014d
2oothpick <106851355+2oothpick@users.noreply.github.com>
2023-12-13 18:03:46 +0000

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     import requests
     import sys
     from requests.auth import HTTPBasicAuth
-    user = sys.argv[1]
-    passw = sys.argv[2]
+    user, passw = sys.argv[1], sys.argv[2]
     response = requests.get('https://api.github.com/user', auth=(user, passw))
     print(response.json().get('id'))
