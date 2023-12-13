4e2ae41456afcebb3831d0c7325c8e85e13274a5
2oothpick <106851355+2oothpick@users.noreply.github.com>
2023-12-13 07:10:38 +0000

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     import sys
     url = sys.argv[1]
     email = {"email": sys.argv[2]}
-    r = requests.get(url, data=email)
+    r = requests.post(url, data=email)
     print(r.text())
