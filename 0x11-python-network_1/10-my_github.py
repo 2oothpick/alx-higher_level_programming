b337369c080f16e6e63a0d4bcd0799ac8e11a66d
2oothpick <106851355+2oothpick@users.noreply.github.com>
2023-12-13 18:04:02 +0000

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,6 @@ and uses GitHub api to display id
 if __name__ == '__main__':
     import requests
     import sys
-    from requests.auth import HTTPBasicAuth
     user, passw = sys.argv[1], sys.argv[2]
     response = requests.get('https://api.github.com/user', auth=(user, passw))
     print(response.json().get('id'))
