65dc10fefe711c76fa4a1d9389214c50b7959e82
2oothpick <kobbyboateng14@gmail.com>
2024-01-04 01:12:58 +0000

    Fix semistandard error Handled error and used camelCase for JsObject

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,9 @@ const request = require('request');
 // Make a simple GET request
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
-  // console.log('code:', response && response.statusCode);
-  const JS_object = JSON.parse(body);
-  console.log(JS_object.title);
+  if (error) {
+    console.error('error:', error);
+  }
+  const JsObject = JSON.parse(body);
+  console.log(JsObject.title);
 });
