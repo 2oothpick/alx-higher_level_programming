8b13810867140996d1d8f7a7c046c8f33ab41814
2oothpick <kobbyboateng14@gmail.com>
2024-01-04 00:00:25 +0000

    Fix error handling

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,8 @@
 const request = require('request');
 
 request(process.argv[2], (error, response) => {
-  console.error('error:', error);
+  if (error) {
+    console.error('error:', error);
+  }
   console.log('code:', response && response.statusCode);
 });
