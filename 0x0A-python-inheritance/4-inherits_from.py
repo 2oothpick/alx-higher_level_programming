1bdc84fbbcc329c2e472845bde162703ba126826
2oothpick <106851355+2oothpick@users.noreply.github.com>
2024-03-04 19:27:48 +0000

    Fix pycodestyle compare error
    Use `is` for exact checks

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ this here module contains the inherits_from function
 
 def inherits_from(obj, a_class):
     """returns true if the object is an instance of a subclass """
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
