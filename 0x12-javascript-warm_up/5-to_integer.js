c828fe49145a102f47065fbe0cc67fbea0a01e95
2oothpick <kobbyboateng14@gmail.com>
2024-05-05 10:18:04 +0000

    Refactor 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const x = Number(process.argv[2]);
-if (isNaN(x)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${x}`);
-}
+const num = Number(process.argv[2]);
+
+isNaN(num[0]) ? console.log('Not a number') : console.log(`My number: ${num[0]}`);
